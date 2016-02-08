b82634abde83a0939e17054d83b90e2ae975ac40
clarebhamilton <clarebhamilton@hotmail.com>
2016-02-08 22:59:06 +0000

    Google analytics added

--- doPublishSite.sh
+++ doPublishSite.sh
@@ -2,5 +2,5 @@
 
 jekyll build --source blog_working_folder --destination public_html/blog
 
-
-#lftp command
+#Push to live website
+lftp -e "mirror -R /home/ubuntu/workspace/public_html/ /ormskirkmusicsociety.org.uk && exit" -u $FTPUSER,$FTPPASS $FTPSITE
